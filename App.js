d79fd2c12abca428d7f41555fd6d66ee858fa2a4
HeXorBUSA <74895927+HeXorBUSA@users.noreply.github.com>
2022-05-11 15:32:57 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -52,7 +52,8 @@ function App() {
       <Alert alert={alert}/>
       <Routes>
           <Route exact path="/about" element={ <About mode={mode} />}/>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> }/>
+          <Route element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> }/>
+          <Route exact path="/TextUtility_demo" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> }/>
       </Routes>
     </>
   );
